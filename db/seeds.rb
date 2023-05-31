e1454dde64a2d86bf151c429f47856cbfe461312
RaineHanley <rainehanley@gmail.com>
2023-05-30 18:37:05 -0700

    initial commit with 3 pets added to seeds

---
+++ db/seeds.rb
@@ -0,0 +1,12 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
+#   Character.create(name: "Luke", movie: movies.first)
+Pet.create(name: "Lucy", breed: "Greyhound Mix", image: "https://scontent.fhio2-1.fna.fbcdn.net/v/t39.30808-6/347127758_613411284049082_8799047038250609284_n.jpg?stp=cp6_dst-jpg&_nc_cat=108&ccb=1-7&_nc_sid=8bfeb9&_nc_ohc=DpEIqsmrykEAX8IU2sU&_nc_ht=scontent.fhio2-1.fna&oh=00_AfDAdy2HvPfWVcVYae5C8zSIjVBsAFF702BfKRcA2ZkIrA&oe=647B4F98")
+
+Pet.create(name: "Rune", breed: "Chihuahua Terrier Mix", image: "https://scontent.fhio2-2.fna.fbcdn.net/v/t39.30808-6/347567547_2219514191587124_6415144649774070872_n.jpg?stp=cp6_dst-jpg&_nc_cat=100&ccb=1-7&_nc_sid=0debeb&_nc_ohc=fO6_SHMUDlkAX8gGNEQ&_nc_ht=scontent.fhio2-2.fna&oh=00_AfBcg1ON3qu5ZioCLvm705Vn_eFBfmRZmqf7W3rbOt8MqA&oe=647BFBF6")
+
+Pet.create(name: "Greyson", breed: "Domestic Shorthair", image: "https://scontent.fhio2-2.fna.fbcdn.net/v/t39.30808-6/347842922_711239254088405_5098316311033012561_n.jpg?stp=cp6_dst-jpg&_nc_cat=109&ccb=1-7&_nc_sid=0debeb&_nc_ohc=FmCEUa13648AX8QRJ8d&_nc_ht=scontent.fhio2-2.fna&oh=00_AfCPVONbWYDQ5b7ZrBjeuFz4VV7fMBAU-NzaqNBVknej9w&oe=647B5DA6")
